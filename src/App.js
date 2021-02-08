0c1866970b5da531fc527245dad3e698c7da4913
Jamal Ahmed <j8ahmed@gmail.com>
2021-02-07 19:28:36 -0500

    Updated root App Component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import J8ahmed from './projects/j8ahmed_portfolio'
+import ToursListApp from './tours_list'
 
 const App = () => {
     return (
     <React.Fragment> 
-        <J8ahmed />
+        <ToursListApp />
     </React.Fragment>)
 }
 
